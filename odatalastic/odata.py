60aff42ac4f855a36d204d0f0e03df47f35b3d71
Eugeniy Kalinin <burump@gmail.com>
2014-02-21 17:00:24 -0800

    basic logic operators

--- odatalastic/odata.py
+++ odatalastic/odata.py
@@ -1,2 +1,28 @@
+from pyparsing import (operatorPrecedence, opAssoc, CaselessLiteral,
+                       quotedString, removeQuotes)
+
+
+def _not_expression(tokens):
+    return {'not': tokens[0][-1]}
+
+
+def _and_expression(tokens):
+    return {'and': tokens[0][0::2]}
+
+
+def _or_expression(tokens):
+    return {'or': tokens[0][0::2]}
+
+
+def _query_expression():
+    operand = quotedString.setParseAction(removeQuotes)
+    return operatorPrecedence(operand, [
+        (CaselessLiteral('not'), 1, opAssoc.RIGHT, _not_expression),
+        (CaselessLiteral('and'), 2, opAssoc.LEFT, _and_expression),
+        (CaselessLiteral('or'), 2, opAssoc.LEFT, _or_expression)
+    ])
+
+
 def parse_query(value):
-    pass
+    grammar = _query_expression()
+    return grammar.parseString(value)[0]
--- tests/test_odata.py
+++ tests/test_odata.py
@@ -1,2 +1,48 @@
-def test_dummy():
-    pass
+# -*- coding: utf-8 -*-
+from nose.tools import assert_equal
+from odatalastic.odata import parse_query
+
+
+def test_quoted_string():
+    assert_equal(parse_query('"a"'), 'a')
+    assert_equal(parse_query("'a'"), 'a')
+
+
+def test_unicode_string():
+    assert_equal(parse_query('"панда"'), 'панда')
+
+
+def test_not():
+    assert_equal(parse_query('not "b"'), {'not': 'b'})
+
+
+def test_multiple_not():
+    assert_equal(parse_query('not not "b"'), {'not': {'not': 'b'}})
+
+
+def test_and():
+    assert_equal(parse_query('"a" and "b"'), {'and': ['a', 'b']})
+
+
+def test_multiple_and():
+    assert_equal(parse_query('"a" and "b" and "c"'),
+                 {'and': ['a', 'b', 'c']})
+
+
+def test_or():
+    assert_equal(parse_query('"a" or "b"'),
+                 {'or': ['a', 'b']})
+
+
+def test_multiple_or():
+    assert_equal(parse_query('"a" or "b" or "c"'),
+                 {'or': ['a', 'b', 'c']})
+
+
+def test_operator_associativity():
+    assert_equal(parse_query('("a" and "b") or "c"'),
+                 {'or': [{'and': ['a', 'b']}, 'c']})
+    assert_equal(parse_query('"a" and ("b" or "c")'),
+                 {'and': ['a', {'or': ['b', 'c']}]})
+    assert_equal(parse_query('not ("a" and "b")'),
+                 {'not': {'and': ['a', 'b']}})
